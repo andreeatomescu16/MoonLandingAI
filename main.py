43514c93ee8a262c3cd16cff85545f22200abfbe
andreeatomescu16 <andreeatomescu040@gmail.com>
2024-11-16 14:12:12 +0200

    Delete main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-import torch
-from trl import SFTTrainer
-from peft import LoraConfig
-from datasets import load_dataset
-from transformers import (AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, TrainingArguments, pipeline)
